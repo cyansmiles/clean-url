226c9ab6726d79fd5fee14d4486c32042e7d625d
Nathan Sharfi <cyan.smiles@yahoo.com>
2016-12-12 20:47:51 -0600

    Update clean-url.js

--- clean-url.js
+++ clean-url.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 // url is a test number
-var url = "number=&number=&number=&number=&number=&STREET=&ActionType=QuickAddressSearch&SUB_DIV=Highland+Meadows&ActionType=QuickNeighborhoodSearch&TYPES_OF_HOMES=&SQFT=0&MIN_PRICE=0&MAX_PRICE=10000000&BDRMS=0&BATHS=0&UPDATED_MARKET=1&halfbaths=0&ONLY_OFFICE=YES";
+var url = "number=&number=&number=&number=&number=&STREET=&ActionType=QuickAddressSearch&SUB_DIV=Community+Name&ActionType=QuickNeighborhoodSearch&TYPES_OF_HOMES=&SQFT=0&MIN_PRICE=0&MAX_PRICE=10000000&BDRMS=0&BATHS=0&UPDATED_MARKET=1&halfbaths=0&ONLY_OFFICE=YES";
 
 url = "http://www.whatever.com" + "/test.html?" + url;
 
